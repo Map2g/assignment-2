a3623ae680d1d09f3a06cd15ec64d7ea63ca51ca
Mel Palmer <melpalmer421@gmail.com>
2019-01-31 18:21:43 -0800

    Added implementation for main method

--- BeerSong.java
+++ BeerSong.java
@@ -1,7 +1,19 @@
 public static void main(String args[]){
-
+	Ninety_Nine_Bottle_of_Beer();
 }
 
 public static void Ninety_Nine_Bottles_of_Beer(){
-
+        int num = 99;
+        
+        while (num > 0){
+            if (num == 1){
+                System.out.println(num + " bottle of beer on the wall, " + num +
+                    " bottle of beer\ntake one down, pass it around, no more "
+                            + "bottles of beer on the wall");
+            }
+            
+            System.out.println(num + " bottles of beer on the wall, " + num + 
+                    " bottles of beer\ntake one down, pass it around, " + --num + 
+                    " bottles of beer on the wall");
+        }
 }
